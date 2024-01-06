33293a4fc77305fcf8dbb4dd5a7eb865bcab8edc
vikas gole <129413413+SketchG2001@users.noreply.github.com>
2024-01-06 11:31:55 +0530

    Merge remote-tracking branch 'origin/master'

--- src/Day_3/4.2_rock_paper_game.py
+++ src/Day_3/4.2_rock_paper_game.py
@@ -45,14 +45,15 @@ paper = '''        /')    ./')             ('\.    ('\
 
 game_iamge = [rock,paper,scissor]
 user_choice = int(input("what do you choose? Type 0 for Rock, 1 for paper or 2 for scissors.\n"))
-print(game_iamge[user_choice])
-computer_choice = random.randint(0,2)
+if user_choice >=3 or user_choice<0:
+    print("invalid input")
+else:
+    print(game_iamge[user_choice])
+    computer_choice = random.randint(0,2)
 print("computer_chose:")
 print(game_iamge[computer_choice])
 if user_choice == 0 and computer_choice == 2:
     print("You Win!")
-elif user_choice >=3 or user_choice < 0:
-    print("you typed an invalid number, you lose!")
 elif computer_choice == 2 and user_choice == 2:
     print("you lose")
 elif computer_choice>user_choice:
