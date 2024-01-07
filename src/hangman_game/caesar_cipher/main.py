44b27796b11b13e032e9f32d496e959f32264ee5
vikas gole <129413413+SketchG2001@users.noreply.github.com>
2024-01-07 21:16:38 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/hangman_game/caesar_cipher/main.py
@@ -0,0 +1,33 @@
+alphabet = ['a', 'b', 'c',
+           'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c',
+           'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+from art import logo
+
+print(logo)
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter
+    print(f"The {cipher_direction}d text is {end_text}")
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'Encode' to encrypt, type 'Decode' to decrypt: \n").lower()
+    text = input("Type message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+    shift = shift%26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    result = input("Type 'yes' if you want to go again. otherwise type 'no'.\n").lower()
+    if result == "no":
+        should_continue = False
+        print("good bye")
