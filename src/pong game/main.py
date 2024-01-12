102a280ed584c986b77c7f687feb86257ce10d63
vikas gole <129413413+SketchG2001@users.noreply.github.com>
2024-01-12 14:29:41 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/pong game/main.py
@@ -0,0 +1,43 @@
+import time
+from turtle import  Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(l_paddle.go_down, "d")
+screen.onkey(l_paddle.go_up, "u")
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+#     Detect collision with ball
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    # detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+screen.exitonclick()
